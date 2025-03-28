d62ae381d8e08bd9a94bdf3e57af325a7142b73f
HAROLPERIODICO <haroldiazmelendez@gmail.com>
2025-03-28 16:02:25 -0500

    PastryLand tu aventura empieza ahora

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="HD soluctions, la mejor opcion en Software de gestion empresarial">
+		<meta name="keywords" content="electronica, Software, gestion, empresarial, pagina, web">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>
+			Inicio - PRASTYLAND
+		</title>
+		<link rel="shortcut icon" href="img/logopastrylandsvg.svg">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&family=Sigmar&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/normalize.css">		
+		<link rel="stylesheet" href="style.css">
+		<script src="https://kit.fontawesome.com/2d60844ad5.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="index.html">
+					<img src="img/logopastrylandsvg.svg" class="logo">
+				</a>
+				<nav>
+					<a href="#inicio">Inicio</a>
+					<a href="#nosotros">Nosotros</a>
+					<a href="#servicios">Servicios</a>
+					<a href="#galeria">Galeria</a>
+					<a href="#contactenos">Contactenos</a>
+				</nav>
+					<a href="#" class="hamburguesa"><i class="fa-solid fa-bars"></i></a>
+			</div>
+		</header>
+		<main>
+			<section id="inicio">
+				<img src="img/logopastrylandsvg.svg">
+				<div class="bloque-inicio">
+					<h1>Bienvenidos a PRASTYLAND</h1>
+					<p>
+					"En PRASTYLAND nos dedicamos a crear tortas artesanales únicas, deliciosas y personalizadas para cada ocasión especial."
+					</p>  
+					<a href="#nosotros" class="boton boton-rojo">Ver más</a>
+				</div>
+			</section>
+			<section id="nosotros" class="seccion">
+				<div class="container">
+					<p>"En PRASTYLAND nos dedicamos a crear tortas artesanales únicas, deliciosas y personalizadas para cada ocasión especial. Nuestra misión es endulzar los momentos más importantes de nuestros clientes con productos de alta calidad, utilizando ingredientes frescos y técnicas tradicionales, todo con un toque de creatividad y amor en cada detalle."
+					</p>	
+				</div>
+			</section>
+			<section id="servicios" class="seccion">
+				<div class="container">
+					<div class="row">
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio1.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 1</h3>
+									<p>"Ser reconocidos como la marca líder en la creación de tortas artesanales para fiestas, de todo Malambo y sus alrededores."</p>
+									<a href="#" class="boton boton-blanco ">Ver más</a>	
+								</div>
+							</div>
+						</div>
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio2.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 2</h3>
+									<p>"Ofreciendo experiencias memorables a nuestros clientes a través de la innovación, la excelencia en nuestros productos y un servicio personalizado. "</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>	
+								</div>
+							</div>
+						</div>
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio3.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 3</h3>
+									<p>"Queremos expandirnos, llevando la magia de PRASTYLAND a más celebraciones y creando un legado de sabor y calidad."</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="galeria">
+				<div class="container-fluid">
+					<div class="row">
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+								<h4>Imagen1</h4>					
+							</div>
+						</div>
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio2.jpg">
+								<h4>Imagen2</h4>					
+							</div>						
+						</div>
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio3.jpg">
+								<h4>Imagen3</h4>					
+							</div>						
+						</div>
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio4.jpg">
+								<h4>Imagen4</h4>					
+							</div>						
+						</div>
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio5.jpg">
+								<h4>Imagen5</h4>					
+							</div>						
+						</div>
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio6.jpg">
+								<h4>Imagen6</h4>					
+							</div>						
+						</div>
+					</div>	
+				</div>
+			</section>
+			<section id="contactenos" class="seccion">
+				<iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Pivijay+(Colombia)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+							<form action="index.php" method="post">
+								<div class="from-block">
+									<input type="text" name="nombre" class="form-control" placeholder="Nombre">
+								</div>
+								<div class="from-block">
+									<input type="email" name="email" class="form-control" placeholder="Email">
+								</div>
+								<div class="from-block">
+									<textarea name="mensaje" placeholder="Mensaje"></textarea>
+								</div>
+								<div class="from-block bloque-ultimo">
+									<input type="submit" class="boton boton-negro" value="Enviar">
+								</div>
+								<?php
+
+									if($_SERVER["REQUEST_METHOD"] == "POST")
+									{
+										$nombre = $_POST["nombre"] ;
+										$email = $_POST["email"] ;
+										$mensaje = $_POST["mensaje"] ;
+
+										if(isset($nombre))
+										{
+											if(isset($email))
+											{
+												if(isset($mensaje))
+												{
+													$para = "haroldiazmelendez@gmail.com";
+													$asunto = "PRUEBA WEB PHP";
+													$cuerpo = $nombre."\n".$email."\n".$mensaje;
+													$adicional = "From: noreply@gmail.com";
+
+													mail($para,$asunto,$cuerpo,$adicional);
+												?>
+													<p>Envio exitoso</p>
+												<?php
+												}
+											}
+										}
+									}
+								?>	
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="columna columna-25 columna-mobile-100">
+						<img src="img/logo-blanco.png" class="logo-footer">
+						<p>
+							En PRASTYLAND nos dedicamos a crear tortas artesanales únicas, deliciosas y personalizadas para cada ocasión especial.
+						</p>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Temas relacionados
+						</h3>
+						<ul>
+							<li><a href="#">Tema 1</a></li>
+							<li><a href="#">Tema 2</a></li>
+							<li><a href="#">Tema 3</a></li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Datos de contacto
+						</h3>
+						<ul>
+							<li>haroldiazmelendez@gmail.com</li>
+							<li>57+ 3167543109</li>
+							<li>Carrera 1g # 7 - 11; San Antonio, Malambo, Atlantico, Colombia</li>
+						</ul>						
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Redes sociales
+						</h3>
+						<ul class="redes">
+							<li>
+								<a href="#">
+									<i class="fa-brands fa-square-facebook"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa-brands fa-square-whatsapp"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa-brands fa-square-instagram"></i>
+								</a>
+							</li>
+						</ul>						
+					</div>										
+				</div>
+			</div>
+			<div class="barra-footer">
+				&copy; Derechos reservados HD Software Solution - 2025
+			</div>
+		</footer>
+		<script src="js/jquery.js"></script>
+		<script src="js/funciones.js"></script>		
+	</body>
+</html>
